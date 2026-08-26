8949cf610f4624e967770140dc5f2755690d852f
ricsirogi <rogozsan.richard06@gmail.com>
2026-08-26 19:08:21 +0200

    Add code to load raw JSON files in data_loader_base.py

--- src/base/data_loader_base.py
+++ src/base/data_loader_base.py
@@ -1,6 +1,69 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+import datetime as dt
+import json
+import re
+
+import pandas as pd
+
+from src.data_downloader import DataDownloader
 
 
 class DataLoaderBase(ABC):
     def __init__(self):
+        self.downloader = DataDownloader()
+
+    @abstractmethod
+    def load_data(self) -> None:
+        """
+        Abstract method, every class derived from DataDownloader must overwrite this function
+        :return None:
+        """
+
         pass
+
+    def load_file(self, file_path: str) -> tuple[pd.Timestamp, pd.Timestamp, str, str, pd.DataFrame]:
+        """
+        Reads the data file and extracts the necessary information for data loading from the file name
+        :param str file_path: Path of the file
+        :return tuple[pd.Timestamp, pd.Timestamp, str, str, pd.DataFrame]:
+                start_time, end_time, d_nature, d_type, raw JSON data
+        """
+
+        with open(file_path, 'r+') as file:
+            f_data = json.load(file)
+
+        # Read the data type, station, start time and end time from the file name
+        name_pattern = r'(\S{2})_(\d{6})_(\d{4}-\d{2})_(\d{4}-\d{2})'
+        match = re.search(pattern=name_pattern, string=file_path)
+
+        if match:
+            start_time = pd.to_datetime(match.group(3))
+            end_time = pd.to_datetime(match.group(4))
+
+            return (start_time,
+                    end_time,
+                    match.group(1)[0],
+                    match.group(1)[1],
+                    self.transform_json_data(data=f_data[0]['TsItemList'])
+                    )
+
+        else:
+            raise ValueError("File name does not follow naming format")
+
+    @staticmethod
+    def transform_json_data(data: dict) -> pd.DataFrame:
+        """
+        This function corrects the read time series' time zone
+
+        :param dict data: the data to be transformed
+
+        :return pd.DataFrame: the transformed data
+        """
+
+        ts_list = data
+        raw = pd.DataFrame(ts_list)
+
+        raw['UTCTime'] = pd.to_datetime(raw['UTCTime']).dt.tz_localize(None)
+        raw.set_index('UTCTime', inplace=True)
+
+        return raw
