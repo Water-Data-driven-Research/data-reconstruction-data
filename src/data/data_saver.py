0cfde111d61afcc23ebea59e5df0f0f98a4f4710
ricsirogi <rogozsan.richard06@gmail.com>
2026-08-26 20:24:46 +0200

    Add code to convert and save JSON data in CSV format in data_saver.py

--- src/data/data_saver.py
+++ src/data/data_saver.py
@@ -1,4 +1,25 @@
+import os
+import pandas as pd
+
+
 class DataSaver:
+
     @staticmethod
-    def save_csv():
-        pass
+    def save_csv(df: pd.DataFrame, file_path: str, file_name: str, include_index: bool = True):
+        """Saves a pandas DataFrame to a CSV file on disk.
+
+        :param pd.DataFrame df: The transformed DataFrame to save
+        :param str file_path: Destination file path
+        :param str file_name: Destination file name
+        :param bool include_index: Whether to write row names (index) into the CSV
+        """
+
+        corrected_file_name = file_name.removesuffix(".json") + ".csv"
+
+        # Ensure directory exists before saving
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+
+        full_path = os.path.join(file_path, corrected_file_name)
+
+        # Write DataFrame directly to disk
+        df.to_csv(full_path, index=include_index)
