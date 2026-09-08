17e500d22ff8c06f678bc1c2713d686c3fd42a82
ricsirogi <rogozsan.richard06@gmail.com>
2026-09-08 17:50:02 +0200

    Rename testing files

--- src/tests/test_data_downloader.py
+++ src/tests/test_data_downloader.py
@@ -18,7 +18,7 @@ def download_context():
     downloader = DataDownloader(file_url=test_url, file_name=file_name)
 
     context = {
-        "data_folder": downloader.data_folder,  # Path: the path of the downloaded folder
+        "dest_path": downloader.dest_path,  # Path: the path of the downloaded folder
         "test_url": test_url,  # str: the url of the zip file downloaded
         "file_name": file_name,  # str: name of the zip file downloaded
     }
@@ -26,8 +26,8 @@ def download_context():
     yield context
 
     # Delete the extracted data after all tests run
-    if downloader.data_folder.exists():
-        shutil.rmtree(downloader.data_folder)
+    if downloader.dest_path.exists():
+        shutil.rmtree(downloader.dest_path)
 
 
 def test_download_directory_exists(download_context: dict):
@@ -37,10 +37,10 @@ def test_download_directory_exists(download_context: dict):
     :param dict download_context: data related to the download
     """
 
-    data_folder = download_context["data_folder"]
+    dest_path = download_context["dest_path"]
 
-    assert data_folder.exists(), "The downloaded folder does not exist."
-    assert data_folder.is_dir(), "The downloaded path is not a directory."
+    assert dest_path.exists(), "The downloaded folder does not exist."
+    assert dest_path.is_dir(), "The downloaded path is not a directory."
 
 
 def test_expected_subdirectories_exist(download_context: dict):
@@ -59,10 +59,10 @@ def test_expected_subdirectories_exist(download_context: dict):
         "reconstructed_data",
     ]
 
-    data_folder = download_context["data_folder"]
+    dest_path = download_context["dest_path"]
 
     for folder_name in expected_folders:
-        folder_path = data_folder / folder_name
+        folder_path = dest_path / folder_name
         assert folder_path.exists(), f"Missing subfolder: {folder_name}"
         assert folder_path.is_dir(), f"Expected {folder_name} to be a directory."
 
@@ -74,19 +74,19 @@ def test_specific_files_exist(download_context: dict):
     :param dict download_context: data related to the download
     """
 
-    data_folder = download_context["data_folder"]
+    dest_path = download_context["dest_path"]
 
     expected_files = [
         # csv_data structure
-        data_folder / "csv_data" / "Szegedhez" / "algyo_vizhozam.csv",
+        dest_path / "csv_data" / "Szegedhez" / "algyo_vizhozam.csv",
         # data structure
-        data_folder / "data" / "tv_210888_2004-01_2010-01.json",
+        dest_path / "data" / "tv_210888_2004-01_2010-01.json",
         # reconstructed_data structure
-        data_folder / "reconstructed_data" / "Szeged" / "Szeged_dl_teszt.json",
+        dest_path / "reconstructed_data" / "Szeged" / "Szeged_dl_teszt.json",
     ]
 
     for file_path in expected_files:
-        assert file_path.exists(), f"Missing file: {file_path.relative_to(data_folder)}"
+        assert file_path.exists(), f"Missing file: {file_path.relative_to(dest_path)}"
         assert file_path.is_file(), f"Expected {file_path.name} to be a file."
         assert file_path.stat().st_size > 0, f"File {file_path.name} is empty."
 
@@ -98,9 +98,9 @@ def test_zip_file_deleted(download_context: dict):
     :param dict download_context: data related to the download
     """
 
-    data_folder = download_context["data_folder"]
+    dest_path = download_context["dest_path"]
     file_name = download_context["file_name"]
 
-    zip_path = data_folder / file_name
+    zip_path = dest_path / file_name
 
     assert not zip_path.exists(), f"Zip file was not deleted: {zip_path}"
