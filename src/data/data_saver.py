d531d583ba7cf20360e2a700ec18605d58624f8f
ricsirogi <rogozsan.richard06@gmail.com>
2026-09-08 16:56:33 +0200

    Add method to create dest folder in data_saver.py

--- src/data/data_saver.py
+++ src/data/data_saver.py
@@ -1,24 +1,44 @@
-import os
+from pathlib import Path
+
 import pandas as pd
+
+from src import data_folder
 
 
 class DataSaver:
+    def __init__(self):
+        pass
 
-    @staticmethod
-    def save_csv(data: pd.DataFrame, file_path: str, file_name: str, include_index: bool = True):
+    def save_csv(self, data: pd.DataFrame, file_name: str, include_index: bool = True):
         """Saves a pandas DataFrame to a CSV file on disk.
 
         :param pd.DataFrame data: The transformed DataFrame to save
-        :param str file_path: Destination file path
         :param str file_name: Destination file name
         :param bool include_index: Whether to write row names (index) into the CSV
         """
 
-        corrected_file_name = file_name.removesuffix(".json") + ".csv"
-        full_path = os.path.join(file_path, corrected_file_name)
+        full_path = self.make_folder(file_name=file_name, extension="csv")
 
-        # Ensure directory exists before saving
-        os.makedirs(file_path, exist_ok=True)
+        # Convert Series to DataFrame and write it to disk
+        pd.DataFrame(data).to_csv(full_path, index=include_index)
 
-        # Write DataFrame directly to disk
-        data.to_csv(full_path, index=include_index)
+    @staticmethod
+    def make_folder(file_name: str, extension: str) -> Path:
+        """
+        Method for creating a folder for the given extension's download destination and for creating a full path
+
+        :param str file_name: Original name of the file (doesn't matter if the extension is included)
+        :param str extension: The desired extension of the file e.g.: csv (without dot)
+
+        :return Path full_path: The exact path of the saved file
+                that includes the given file name e.g.: data/csv/example.csv
+        """
+
+        new_name = file_name.removesuffix(".json") + "." + extension
+
+        dest_path = data_folder / extension
+        dest_path.mkdir(parents=True, exist_ok=True)
+
+        full_path = data_folder / extension / new_name
+
+        return full_path
