15bcabfb9fa253553e22b72de912cb58ee6b6b8d
ricsirogi <61251735+ricsirogi@users.noreply.github.com>
2026-09-10 22:04:26 +0200

    Dev/data downloader fix (#7)
    * implement DataDownloader module
    
    * Add pytest test file for DataDownloader
    
    * add Dockerfile and GitHub Action workflow for DataDownloader tests
    
    * Add empty line at the end of requirements.txt
    
    * Update .gitignore README.md and requirements.txt
    
    * Fix output path and type annotation in data_downloader.py
    
    * Change when test is triggered
    
    * Remove global variables from data_downloader_test.py
    
    * Add an importable path to the module
    
    * Tweak Dockerfile package imports
    
    * Add newlines after docstrings in data_downloader.py
    
    * Add newline after import in src/__init__.py
    
    * Fix Dockerfile_data_downloader_test
    
    * Fix data_downloader_test.py
    
    * Rename test files

--- src/data_downloader.py
+++ src/data_downloader.py
@@ -1,3 +1,54 @@
+import gc
+import zipfile
+
+import gdown
+
+from src import data_folder
+
+
 class DataDownloader:
-    def __init__(self):
-        pass
+    """
+    Class for downloading time series related data
+    """
+
+    def __init__(self, file_url: str, file_name: str):
+        """
+        Downloads the data, unzips it and deletes the original zip file
+
+        :param str file_url: The Google Drive url of the file to be downloaded
+        :param str file_name: The name of the file to be downloaded
+        """
+
+        self.file_path = data_folder / file_name
+        data_folder.mkdir(parents=True, exist_ok=True)
+
+        self.download(file_url=file_url)
+
+        self.unzip()
+
+    def download(self, file_url: str):
+        """
+        Downloads the data from Google Drive
+        :param str file_url: The Google Drive url of the file to be downloaded
+        """
+
+        if not self.file_path.is_file():
+            gdown.download(url=file_url,
+                           output=str(self.file_path),
+                           quiet=False)
+
+    def unzip(self):
+        """
+        Unzips the downloaded zip file and deletes the original zip file
+        """
+
+        if self.file_path.suffix == ".zip" and self.file_path.is_file():
+            with zipfile.ZipFile(self.file_path, "r") as zip_ref:
+                zip_ref.extractall(data_folder)
+
+            # Force Python to release open file handles left by gdown
+            gc.collect()
+            try:
+                self.file_path.unlink()  # Delete the original zip file
+            except PermissionError:
+                print(f"Warning: Could not remove {self.file_path} due to locked handle.")
